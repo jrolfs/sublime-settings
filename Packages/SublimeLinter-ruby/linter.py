cc5a1c20866c769ffa0d91fb77d91c09bd38bad5
Jamie Rolfs <jamie.rolfs@gmail.com>
2014-01-08 10:27:29 -0800

    Updated to new SublimeLinter3 settings, added Glyphicons gutter theme, removed SideBarEnhancements and View In Browser
    (cherry picked from commit 139fa0c5669ca026cdcfda1da5c69cdd303d09ef)

---
+++ Packages/SublimeLinter-ruby/linter.py
@@ -0,0 +1,28 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Aparajita Fishman
+# Copyright (c) 2013 Aparajita Fishman
+#
+# License: MIT
+#
+
+"""This module exports the Ruby plugin class."""
+
+from SublimeLinter.lint import RubyLinter
+
+
+class Ruby(RubyLinter):
+
+    """Provides an interface to ruby -wc."""
+
+    syntax = ('ruby', 'ruby on rails', 'rspec', 'html (rails)')
+    cmd = 'ruby -wc'
+    regex = (
+        r'^.+?:(?P<line>\d+): (?:(?P<error>.*?error)|(?P<warning>warning))[,:] (?P<message>[^\r\n]+)\r?\n'
+        r'(?:^[^\r\n]+\r?\n^(?P<col>.*?)\^)?'
+    )
+    multiline = True
+    selectors = {'html (rails)': 'source.ruby.rails.embedded.html'}
+    comment_re = r'\s*#'
